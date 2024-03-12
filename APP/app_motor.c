35841f91fc50febbbbbf551a007cbc9d337104b8
Alipay <zhpwap@qq.com>
2024-03-12 23:23:17 +0800

    写app_motor到一半，觉得应该把motor作为一整个对象使用，而不是监控结构体+pid结构体，这两个应该同属于motor，准备重构，备份一下

--- APP/app_motor.c
+++ APP/app_motor.c
@@ -1,5 +1,45 @@
-//
-// Created by zhpwa on 2024/3/12.
-//
+/**
+  ******************************************************************************
+  * @file           : app_motor.c
+  * @author         : zhpwa
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/3/12
+  ******************************************************************************
+  */
+
 
 #include "app_motor.h"
+
+__weak void app_motor_offline_callback ()
+{
+
+}
+
+/**
+ * @brief 电机监控结构体数组
+ */
+app_motor_t motor_mon[8] = {0};
+
+void app_motor_online ()
+{
+
+}
+
+bool app_motor_online_detect (app_motor_t *mon)
+{
+  uint32_t tick_to_wait =
+#if APP_MOTOR_USE_DEFAULT_WAIT_TICK == 1
+	  APP_MOTOR_DEFAULT_WAIT_TICK;
+#else
+  mon->online.wait_tick;
+#endif
+
+  if (get_app_motor_tick - mon->online.last_tick > tick_to_wait)
+	{
+	  mon->online.online = false;
+	}
+
+  mon->online.last_tick = get_app_motor_tick;
+  return mon->online.online;
+}
--- APP/app_motor.h
+++ APP/app_motor.h
@@ -1,15 +1,44 @@
-//
-// Created by zhpwa on 2024/3/12.
-//
 /**
- * @file app_motor.h
- * @brief 电机应用层头文件
- * @details 包含电机在线检测、电机温度监测
- * @date 2024-3-12 13:36:26
- */
+  ******************************************************************************
+  * @file           : app_motor.h
+  * @author         : zhpwa
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/3/12
+  ******************************************************************************
+  */
+
+
 #ifndef _APP_MOTOR_H_
 #define _APP_MOTOR_H_
 
-//todo 电机保护
+#include "stm32f4xx_hal.h"
+
+#include "stdbool.h"
+
+#define APP_MOTOR_USE_DEFAULT_WAIT_TICK 1
+#define APP_MOTOR_DEFAULT_WAIT_TICK 10U
+
+/**
+ * @brief app_motor结构体
+ * @var last_tick 123
+ */
+typedef struct {
+  struct {
+	uint32_t last_tick;
+	uint32_t wait_tick;
+	bool online;
+  }
+	  online;
+
+  struct {
+	uint32_t overheat_count;//
+	uint32_t temp_lim;//温度上限
+	bool overheat;
+  }
+	  ovh;
+} app_motor_t;
+
+#define get_app_motor_tick HAL_GetTick()
 
 #endif //_APP_MOTOR_H_
